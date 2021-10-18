1138145e2bc61b2f3b133500f37c98df10d68d53
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-18 17:50:44 +0300

    Modify Display component to take in result as string

--- src/components/Display.js
+++ src/components/Display.js
@@ -1,5 +1,19 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-const Display = props => {
-  
+const Display = ({result = '0'}) => {
+  return (
+    <div>
+      <h1>{result}</h1>
+    </div>
+  )
 }
+
+Display.propTypes = {
+  result: PropTypes.string.isRequired
+}
+
+export default Display;
+
+
+
