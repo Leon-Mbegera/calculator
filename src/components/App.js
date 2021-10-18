dc2e7b14f4655ffd6e104bebed70b43e93613281
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-18 16:39:20 +0300

    Implement the App component

---
+++ src/components/App.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const App = () => {
+  return (
+    <React.Fragment>
+      <Display />
+      <ButtonPanel />
+    </React.Fragment>
+  )
+}
