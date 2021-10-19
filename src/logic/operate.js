8a422a32ed38f92a9d721cda0f2d0a24d1b0d97c
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-19 13:25:56 +0300

    Implement logic in operate module and export the logic

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,12 +1,26 @@
-import React from 'react'
 import FatTony from 'big.js'
 
-const operate = (numberOne, numberTwo, operation) => {
+const Operate = (numberOne, numberTwo, operation) => {
   let output;
   let numberOne = new FatTony(numberOne)
   let numberTwo = new FatTony(numberTwo)
 
-  
-
-
+  if (operation === '-') {
+    output = numberOne - numberTwo
+  } else if (operation === '+') {
+    output = numberOne + numberTwo
+  } else if (operation === 'x') {
+    output = numberOne * numberTwo
+  } else if (operation === '÷') {
+    output = numberOne / numberTwo
+  } else if (operation === '%') {
+    if (numberOne) {
+      output = numberOne * 0.01
+    } else if (numberTwo) {
+      output = numberTwo * 0.01
+    }
+  }
+  return output;
 }
+
+export default Operate;
