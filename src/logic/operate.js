6bd84b3bd24233e23127f33afff3446e801fece6
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-19 13:13:12 +0300

    Define operate function and translate passed parameters

---
+++ src/logic/operate.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import FatTony from 'big.js'
+
+const operate = (numberOne, numberTwo, operation) => {
+  let output;
+  let numberOne = new FatTony(numberOne)
+  let numberTwo = new FatTony(numberTwo)
+
+  
+
+
+}
