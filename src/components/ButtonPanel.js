6d92b2dfffcaf1e706e8ebbba3785c1aed68b18f
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-20 17:19:19 +0300

    Refactor ButtonPanel to functional component

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,48 +1,41 @@
 import React from 'react';
 import Button from './Button';
 
-class ButtonPanel extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.handleClick = this.handleClick.bind(this);
-  }
-
-  render() {
-    return (
+const ButtonPanel = (props) => {
+  const { clickHandler } = props;
+  return (
+    <div>
       <div>
-        <div>
-          <Button buttonName="AC" clickHandler={clickHandler} />
-          <Button buttonName="+/-" clickHandler={clickHandler} />
-          <Button buttonName="%" clickHandler={clickHandler} />
-          <Button buttonName="÷" clickHandler={clickHandler} />
-        </div>
-        <div>
-          <Button buttonName="7" clickHandler={clickHandler} />
-          <Button buttonName="8" clickHandler={clickHandler} />
-          <Button buttonName="9" clickHandler={clickHandler} />
-          <Button buttonName="x" clickHandler={clickHandler} />
-        </div>
-        <div>
-          <Button buttonName="4" clickHandler={clickHandler} />
-          <Button buttonName="5" clickHandler={clickHandler} />
-          <Button buttonName="6" clickHandler={clickHandler} />
-          <Button buttonName="-" clickHandler={clickHandler} />
-        </div>
-        <div>
-          <Button buttonName="1" clickHandler={clickHandler} />
-          <Button buttonName="2" clickHandler={clickHandler} />
-          <Button buttonName="3" clickHandler={clickHandler} />
-          <Button buttonName="+" clickHandler={clickHandler} />
-        </div>
-        <div>
-          <Button buttonName="0" clickHandler={clickHandler} />
-          <Button buttonName="." clickHandler={clickHandler} />
-          <Button buttonName="=" clickHandler={clickHandler} />
-        </div>
+        <Button buttonName="AC" handleClick={clickHandler} />
+        <Button buttonName="+/-" handleClick={clickHandler} />
+        <Button buttonName="%" handleClick={clickHandler} />
+        <Button buttonName="÷" handleClick={clickHandler} />
       </div>
-    );
-  }
-}
+      <div>
+        <Button buttonName="7" handleClick={clickHandler} />
+        <Button buttonName="8" handleClick={clickHandler} />
+        <Button buttonName="9" handleClick={clickHandler} />
+        <Button buttonName="x" handleClick={clickHandler} />
+      </div>
+      <div>
+        <Button buttonName="4" handleClick={clickHandler} />
+        <Button buttonName="5" handleClick={clickHandler} />
+        <Button buttonName="6" handleClick={clickHandler} />
+        <Button buttonName="-" handleClick={clickHandler} />
+      </div>
+      <div>
+        <Button buttonName="1" handleClick={clickHandler} />
+        <Button buttonName="2" handleClick={clickHandler} />
+        <Button buttonName="3" handleClick={clickHandler} />
+        <Button buttonName="+" handleClick={clickHandler} />
+      </div>
+      <div>
+        <Button buttonName="0" handleClick={clickHandler} />
+        <Button buttonName="." handleClick={clickHandler} />
+        <Button buttonName="=" handleClick={clickHandler} />
+      </div>
+    </div>
+  );
+};
 
 export default ButtonPanel;
