462e794414429a7fa6d2cd2bed1df56fd06b48b5
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-20 12:19:13 +0300

    Constructor App components and initialize its state with three properties

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,17 @@ import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculate from '../logic/calculate';
 
-const App = () => (
-  <>
-    <Display showing={Calculate} />
-    <ButtonPanel />
-  </>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+}
 
 export default App;
