629f03e29f95a927f03beca227c2941e525da3c3
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-21 17:21:39 +0300

    Modify calculator working logic

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -10,48 +10,74 @@ const Calculate = (dataObject, buttonName) => {
     total = null;
     next = null;
     operation = null;
-  } else if (buttonName === '+/-') {
-    total *= -1;
-  } else if (buttonName === '%') {
-    total *= 0.01;
+  }
+
+  if (buttonName === '+/-') {
+    total = `${(total *= 1)}`;
+    next *= 1;
+  }
+
+  if (buttonName === '%') {
+    total = (total *= 0.01).toString();
+  }
+
+  if (buttonName === '=') {
+    total = Operate(total, next, operation);
+    next = null;
+    operation = null;
   }
 
   if (buttonName === '.') {
     if (!total) {
       total = '0.';
-    } else if (total && operation) {
-      total += '.';
-    } else if (total && next && operation) {
-      next += '.';
-    } else if (total && operation && !next) {
-      next = '0.';
+    }
+
+    if (total && !next) {
+      if (operation) {
+        next += '0.';
+      } else if (total.indexOf('.') === -1) {
+        total += '.';
+      }
+    }
+
+    if (total && next && operation) {
+      if (next.indexOf('.') === -1) {
+        next += '.';
+      }
     }
   }
 
   if (digits.includes(buttonName)) {
-    if (operation === null) {
+    if (operation) {
       if (total === null) {
+        total = buttonName;
+      } else if (typeof total === 'number') {
         total = buttonName;
       } else {
         total += buttonName;
       }
-    } else {
+    } else if (next === null) {
       next = buttonName;
+    } else {
+      next += buttonName;
     }
   }
 
   if (sign.includes(buttonName)) {
     if (!total) {
       total = '0';
-    } else if (total && !next) {
+    }
+
+    if (total && !next) {
       operation = buttonName;
-    } else if (total && next && operation) {
+    }
+
+    if (total && next && operation) {
       total = Operate(total, next, operation);
       next = null;
       operation = buttonName;
     }
   }
-
   return { total, next, operation };
 };
 
