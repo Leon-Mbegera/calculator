e7b7156a885fa3282a92fb55ac30e963fe6ab616
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-22 15:33:29 +0300

    Commencement of dubugging

--- src/logic/calculate.js
+++ src/logic/calculate.js
@@ -5,6 +5,7 @@ const Calculate = (dataObject, buttonName) => {
 
   const digits = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
   const sign = ['-', 'x', '÷', '+'];
+  let reset = false;
 
   if (buttonName === 'AC') {
     total = null;
@@ -14,7 +15,7 @@ const Calculate = (dataObject, buttonName) => {
 
   if (buttonName === '+/-') {
     total = `${(total *= 1)}`;
-    next *= 1;
+    next = total;
   }
 
   if (buttonName === '%') {
