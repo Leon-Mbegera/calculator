862178688ed54c0e232cbdee1a8207ff1e968ac9
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-23 13:54:08 +0300

    Additional logic to output negative result

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -6,7 +6,11 @@ const Operate = (numOne, numTwo, operation) => {
   const numberTwo = new FatTony(numTwo);
 
   if (operation === '-') {
-    output = numberOne.minus(numberTwo);
+    if (numberOne >= numberTwo) {
+      output = numberOne.minus(numberTwo);
+    } else {
+      output = -numberTwo.minus(numberOne);
+    }
   } else if (operation === '+') {
     output = numberOne.plus(numberTwo);
   } else if (operation === 'x') {
