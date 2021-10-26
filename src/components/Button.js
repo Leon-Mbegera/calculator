99d0f5b992edcb40f430d58bef2624887611c41d
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-26 17:34:38 +0300

    Style and position in button panel

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './calc.css';
 
 function Button({ show, onClick }) {
   return (
     <>
-      <button type="button" onClick={onClick}>
+      <button type="button" onClick={onClick} className="button">
         {show}
       </button>
     </>
