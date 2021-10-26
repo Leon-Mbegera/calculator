3c0db027adecd83e856ababcb3143dbcc068da3c
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-26 12:42:16 +0300

    Refactor components to use Hooks and functional components

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import Calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
+function App() {
+  const [state, setState] = useState({
+    total: '',
+    next: '',
+    operation: '',
+  });
 
-    this.state = {
-      total: '',
-      next: '',
-      operation: '',
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+  const handleClick = (buttonName) => {
+    const { total, next, operation } = state;
+    setState(Calculate({ total, next, operation }, buttonName));
+  };
 
-  handleClick(buttonName) {
-    const { total, next, operation } = this.state;
-    const data = Calculate({ total, next, operation }, buttonName);
-    this.setState({
-      total: data.total,
-      next: data.next,
-      operation: data.operation,
-    });
-  }
-
-  render() {
-    const { total } = this.state;
-    return (
-      <>
-        <Display result={total} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </>
-    );
-  }
+  const { total } = state;
+  return (
+    <>
+      <Display result={total} />
+      <ButtonPanel clickHandler={handleClick} />
+    </>
+  );
 }
 
 export default App;
