6d29d83c2d19298ae0623cb98581533aadfbdf6e
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-10-27 12:38:26 +0300

    home page with message about math-magicians

---
+++ src/components/Home.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Home = () => (
+  <div>
+    <div>
+      <h1>Welcome to our page!</h1>
+    </div>
+    <div>
+      <p>
+        Math-magicians is a fictional company based in France.
+        They have requested your help to build a simple product
+        that would assist them with their calculations.
+        An important thing to notice is that they have asked
+        for a product that is just fully-functional and it
+        does not need to be beautiful and user-friendly (at least in its first version).
+      </p>
+    </div>
+  </div>
+);
+
+export default Home;
