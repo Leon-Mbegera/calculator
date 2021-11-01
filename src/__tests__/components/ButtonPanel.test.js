d6ff525c1a68db36ba9b90bb50285831a376255a
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-11-01 20:11:38 +0300

    Modify test case for button panel

--- src/__tests__/components/ButtonPanel.test.js
+++ src/__tests__/components/ButtonPanel.test.js
@@ -2,6 +2,7 @@ import renderer from 'react-test-renderer';
 import ButtonPanel from '../../components/ButtonPanel';
 
 test('renders Display and ButtonPanel components', () => {
-  const snap = renderer.create(<ButtonPanel />).toJSON();
+  const clickHandler = jest.fn();
+  const snap = renderer.create(<ButtonPanel clickHandler={clickHandler} />).toJSON();
   expect(snap).toMatchSnapshot();
 });
