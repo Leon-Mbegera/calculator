c2af8563db7dc6ce8f04e9787f37dcf5323c3ea5
Leon-Mbegera <mbegeraleon89@gmail.com>
2021-11-01 18:32:30 +0300

    snapshot for ButtonPanel component

--- src/__tests__/components/ButtonPanel.test.js
+++ src/__tests__/components/ButtonPanel.test.js
@@ -1,16 +1,7 @@
 import renderer from 'react-test-renderer';
 import ButtonPanel from '../../components/ButtonPanel';
 
-test('Button panel displays all calculator buttons', () => {
-  const onOperationClick = jest.fn();
-  const onNumberClick = jest.fn();
-  const tree = renderer
-    .create(
-      <ButtonPanel
-        onOperationClick={onOperationClick}
-        onNumberClick={onNumberClick}
-      />,
-    )
-    .toJSON();
-  expect(tree).toMatchSnapshot();
+test('renders Display and ButtonPanel components', () => {
+  const snap = renderer.create(<ButtonPanel />).toJSON();
+  expect(snap).toMatchSnapshot();
 });
